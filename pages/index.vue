9455045f31ea08d38937aa4883dc91c1ea4c6228
lm10 <liaoming010@gmail.com>
2024-12-07 10:14:42 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="w-full h-screen flex flex-col items-center justify-center bg-gradient-to-br from-blue-400 via-purple-400 to-pink-400"
+  >
+    <h1 class="text-2xl font-bold text-gray-900">独立开发者Lm10正式出发✈️</h1>
+    <p class="text-gray-600 my-4">我们正在开发一系列的工具</p>
+    <p>
+      <img class="w-96 rounded-2xl" src="/img1.jpg" alt="" />
+    </p>
+    <UButton
+      class="px-6 mt-4"
+      color="primary"
+      variant="solid"
+      to="/wait-list"
+      label="Button"
+      >立即关注！👍</UButton
+    >
+  </div>
+</template>
